f1f22a0856faf24ff09c0c1d637a54cb27c782e7
james <jamesxalbuquerque@gmail.com>
2025-12-01 16:43:50 -0300

    Implements test and service improviments

--- core/beer/service.go
+++ core/beer/service.go
@@ -1,38 +1,140 @@
 package beer
 
+import (
+	"database/sql"
+	"fmt"
+
+	_ "github.com/mattn/go-sqlite3"
+)
+
+// define a interface com as funções que serão usadas pelo restante do projeto
 type UseCase interface {
-	//Retorna um array que dentro de cada posicção existe a posição da memoria de cada item do array
 	GetAll() ([]*Beer, error)
-	GetByID(Id int) (*Beer, error)
+	Get(ID int64) (*Beer, error)
 	Store(b *Beer) error
 	Update(b *Beer) error
-	Remove(Id int) error
+	Remove(ID int64) error
 }
 
-type Service struct{}
-
-func NewService() *Service {
-	// & retorna o endereço de memória do objeto Service
-	return &Service{}
+// a struct Service agora tem uma conexão com o banco de dados dentro dela
+type Service struct {
+	DB *sql.DB
 }
 
+// esta função retorna um ponteiro em memória para uma estrutura
+// a função agora recebe uma conexão com o banco de dados
+func NewService(db *sql.DB) *Service {
+	return &Service{
+		DB: db,
+	}
+}
+
+// vamos implementar as funções na próxima etapa
 func (s *Service) GetAll() ([]*Beer, error) {
-	//  
-	return nil, nil
+	//result é um slice de ponteiros do tipo Beer
+	var result []*Beer
+
+	//vamos sempre usar a conexão que está dentro do Service
+	rows, err := s.DB.Query("select id, name, type, style from beer")
+	//se existe erro a função deve retorná-lo e ele vai ser tratado
+	//por quem chamou o pacote. Essa é uma boa prática em Go
+	if err != nil {
+		return nil, err
+	}
+	//a função defer garante que o comando rows.Close vai ser executado na saída da função
+	//desta forma não precisamos nos preocupar em fechar a conexão
+	defer rows.Close()
+	for rows.Next() {
+		var b Beer
+		err = rows.Scan(&b.ID, &b.Name, &b.Type, &b.Style)
+		if err != nil {
+			return nil, err
+		}
+		//o comando append adiciona novos itens a um slice, sempre no final
+		result = append(result, &b)
+	}
+	return result, nil
 }
 
-func (s *Service) GetByID(Id int) (*Beer, error) {
-	return nil, nil
-}
+func (s *Service) Get(ID int64) (*Beer, error) {
+	//b é um tipo Beer
+	var b Beer
 
+	//o comando Prepare verifica se a consulta está válida
+	stmt, err := s.DB.Prepare("select id, name, type, style from beer where id = ?")
+	if err != nil {
+		return nil, err
+	}
+	defer stmt.Close()
+	err = stmt.QueryRow(ID).Scan(&b.ID, &b.Name, &b.Type, &b.Style)
+	if err != nil {
+		return nil, err
+	}
+	//deve retornar a posição da memória de b
+	return &b, nil
+}
 func (s *Service) Store(b *Beer) error {
+	//iniciamos uma transação
+	tx, err := s.DB.Begin()
+	if err != nil {
+		return err
+	}
+	stmt, err := tx.Prepare("insert into beer(id, name, type, style) values (?,?,?,?)")
+	if err != nil {
+		return err
+	}
+	defer stmt.Close()
+	//o comando Exec retorna um Result, mas não temos interesse nele, por isso podemos ignorá-lo com o _
+	_, err = stmt.Exec(b.ID, b.Name, b.Type, b.Style)
+	if err != nil {
+		tx.Rollback()
+		return err
+	}
+	tx.Commit()
 	return nil
 }
-
 func (s *Service) Update(b *Beer) error {
+	if b.ID == 0 {
+		//podemos também retornar um erro de aplicação
+		//que criamos para definir uma condição de erro, como um possível update sem Where
+		return fmt.Errorf("invalid ID")
+	}
+
+	tx, err := s.DB.Begin()
+	if err != nil {
+		return err
+	}
+	stmt, err := tx.Prepare("update beer set name=?, type=?, style=? where id=?")
+	if err != nil {
+		return err
+	}
+	defer stmt.Close()
+	//o comando Exec retorna um Result, mas não temos interesse nele, por isso podemos ignorá-lo com o _
+	_, err = stmt.Exec(b.Name, b.Type, b.Style, b.ID)
+	if err != nil {
+		tx.Rollback()
+		return err
+	}
+	tx.Commit()
 	return nil
 }
+func (s *Service) Remove(ID int64) error {
+	if ID == 0 {
+		//podemos também retornar um erro de aplicação
+		//que criamos para definir uma condição de erro, como um possível update sem Where
+		return fmt.Errorf("invalid ID")
+	}
 
-func (s *Service) Remove(Id int) error {
+	tx, err := s.DB.Begin()
+	if err != nil {
+		return err
+	}
+	//o comando Exec retorna um Result, mas não temos interesse nele, por isso podemos ignorá-lo com o _
+	_, err = tx.Exec("delete from beer where id=?", ID)
+	if err != nil {
+		tx.Rollback()
+		return err
+	}
+	tx.Commit()
 	return nil
 }
